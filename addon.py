76430ce5f7b57aace72cd760cb56604630175c5b
Trevor Sullivan <trevor@trevorsullivan.net>
2020-06-07 23:11:54 -0700

    Create addon.py

---
+++ addon.py
@@ -0,0 +1,33 @@
+import bpy
+from bpy.props import StringProperty
+
+bl_info = {
+    'name': "Trevor's Blender Add-on",
+    'blender': (2,80,0),
+    'category': 'Commands'
+}
+
+class OperatorGoToFrame(bpy.types.Operator):
+    bl_idname = 'scene.gotoframe'
+    bl_label = 'Go / jump to specific frame'
+    bl_property = 'frame'
+    
+    frame: StringProperty(default = '1', name = "Frame number")
+    
+    def execute(self, context):
+        self.report({'INFO'}, 'Set frame number to {}'.format(self.frame))
+        context.scene.frame_set(int(self.frame))
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
+
+def register():
+    bpy.utils.register_class(OperatorGoToFrame)
+
+def unregister():
+    bpy.utils.unregister_class(OperatorGoToFrame)
+
+if __name__ == '__main__':
+    register()
